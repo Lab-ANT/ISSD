a2df87096a4ee8b9f20787d72b138b9fdc91d84b
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-05-27 01:11:52 +0800

    add automatic script

--- tasks/run_all.sh
+++ tasks/run_all.sh
@@ -1,6 +1,5 @@
-bash tasks/del_selection_results.sh
-bash tasks/run_selection.sh
-bash nohup tasks/task_time2state.sh > output/tasks_time2state.out &
-bash nohup tasks/task_e2usd.sh > output/tasks_e2usd.out &
-bash nohup tasks/task_ticc.sh > output/tasks_ticc.out &
-bash nohup tasks/task_AutoPlait.sh > output/tasks_AutoPlait.out &
+# sequentially run all tasks, waiting for each to finish before starting the next one
+bash tasks/task_autoplait.sh > output/tasks_autoplait.out
+bash tasks/task_time2state.sh > output/tasks_time2state.out
+bash tasks/task_e2usd.sh > output/tasks_e2usd.out
+bash tasks/task_ticc.sh > output/tasks_ticc.out
--- tasks/task_autoplait.sh
+++ tasks/task_autoplait.sh
@@ -2,12 +2,13 @@
 method_list=(issd pca umap ecs ecp lda sfm)
 dataset_list=(MoCap ActRecTut PAMAP2 USC-HAD SynSeg)
 
+python downstream_methods/AutoPlait/experiments/convert_to_AutoPlait_format.py
 cd downstream_methods/AutoPlait
-python experiments/convert_to_AutoPlait_format.py
 for dataset in ${dataset_list[@]}; do
     for method in ${method_list[@]}; do
         # echo $method
         bash experiments/run_AutoPlait.sh $dataset $method
     done
 done
-python experiments/rediredt_results.py
+cd ../..
+python downstream_methods/AutoPlait/experiments/rediredt_results.py
