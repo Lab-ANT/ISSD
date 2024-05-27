7be6503b1941418066e1a46a9151a246541af7be
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-05-28 04:11:05 +0800

    rename output fname

--- tasks/run_all.sh
+++ tasks/run_all.sh
@@ -7,16 +7,17 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate selection
 echo "current environment: $CONDA_DEFAULT_ENV"
 
-bash tasks/run_selection.sh
+mkdir -p output
+bash tasks/run_selection.sh > output/log_selection.out
 
 conda activate statecorr
 echo "current environment: $CONDA_DEFAULT_ENV"
 
 # sequentially run all tasks
-bash tasks/task_autoplait.sh > output/task_autoplait.out
-bash tasks/task_time2state.sh > output/task_time2state.out
-bash tasks/task_e2usd.sh > output/task_e2usd.out
-bash tasks/task_ticc.sh > output/task_ticc.out
+bash tasks/task_autoplait.sh > output/log_autoplait.out
+bash tasks/task_time2state.sh > output/log_time2state.out
+bash tasks/task_e2usd.sh > output/log_e2usd.out
+bash tasks/task_ticc.sh > output/log_ticc.out
 
 python downstream_methods/AutoPlait/experiments/redirect_results.py
 
