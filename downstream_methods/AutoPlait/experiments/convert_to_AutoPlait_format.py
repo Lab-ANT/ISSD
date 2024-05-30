49e31bcf3d3fbf14720cabd58525d0a76be79c82
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-05-30 18:10:31 +0800

    add sfs

--- downstream_methods/AutoPlait/experiments/convert_to_AutoPlait_format.py
+++ downstream_methods/AutoPlait/experiments/convert_to_AutoPlait_format.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 
 datasets = ['MoCap', 'SynSeg', 'ActRecTut', 'PAMAP2', 'USC-HAD']
-methods = ['pca', 'umap', 'issd', 'ecp', 'ecs', 'lda', 'sfm', 'mi']
+methods = ['pca', 'umap', 'issd', 'ecp', 'ecs', 'lda', 'sfm', 'mi', 'sfs']
 
 for d in datasets:
     for m in methods:
