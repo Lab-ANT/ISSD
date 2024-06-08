68dd22d6cfa45f88f65b900c73a859e44ff626f5
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-06-09 05:22:47 +0800

    add script for running on specified downstream method

--- tasks/only_issd.sh
+++ tasks/only_issd.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-# list of methods to run
-method_list=(issd)
-dataset_list=(MoCap ActRecTut PAMAP2 USC-HAD SynSeg)
+execution_num=$1
 
-for dataset in ${dataset_list[@]}; do
-    for method in ${method_list[@]}; do
-        # echo $method
-        python -u experiments/run_dmethods.py time2state $dataset $method
-        python -u experiments/run_dmethods.py e2usd $dataset $method
-        python -u experiments/run_dmethods.py ticc $dataset $method
-    done
-done
+# activate conda
+source ~/anaconda3/etc/profile.d/conda.sh
 
+# switch to selection environment
+conda activate selection
+echo "current environment: $CONDA_DEFAULT_ENV"
+
+# run selection
+mkdir -p output
+bash tasks/run_selection.sh > output/log_selection.out
+
+# switch to downstream environment
+conda activate downstream
+echo "current environment: $CONDA_DEFAULT_ENV"
+
+# sequentially run all tasks
+bash tasks/task_autoplait.sh > output/log_autoplait.out &
+bash tasks/task_time2state.sh > output/log_time2state.out &
+wait
+bash tasks/task_e2usd.sh > output/log_e2usd.out &
+bash tasks/task_ticc.sh > output/log_ticc.out &
+
+# redirect results for AutoPlait
+python downstream_methods/AutoPlait/experiments/redirect_results.py
+
+# summary results and plot figures
+bash tasks/draw_figures.sh
+
+bash tasks/archive_execution.sh $execution_num
+bash tasks/del_selection_results.sh
