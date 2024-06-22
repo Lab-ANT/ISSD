9921aae399b21a8f1c9ef4c8c77eedd089163942
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-06-22 21:14:55 +0800

    update figure

--- paper_figures/plot_method_box.py
+++ paper_figures/plot_method_box.py
@@ -3,73 +3,124 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-clf_name_list = ['ISSD', 'PCA', 'UMAP', 'ECP', 'ECS', 'LDA', 'SFM']
-dmethod_names = ['Time2State', 'TICC', 'AutoPlait', 'E2USD']
-dmethod_names_lower = ['time2state', 'ticc', 'autoplait', 'e2usd']
-# dataset_names = ['MoCap', 'ActRecTut', 'PAMAP2', 'USC-HAD', 'Synthetic']
+def convert_name(str):
+    if str == 'time2state':
+        return 'Time2State'
+    elif str == 'ticc':
+        return 'TICC'
+    elif str == 'e2usd':
+        return 'E2USD'
+    elif str == 'autoplait':
+        return 'AutoPlait'
+    else:
+        return str
 
-num_dmethod = len(dmethod_names)
+for metric in ['nmi']:
+    with open(f'output/summary_{metric}.txt', 'r') as f:
+        # retrieve method names
+        lines = f.readlines()
+        method_name_list = lines[1].split('|')[2:-1]
+        method_name_list = [name.strip().upper() for name in method_name_list]
+    with open(f'output/summary_{metric}.txt', 'r') as f:
+        # retrieve downstream method names
+        lines = f.readlines()
+        lines = lines[3:-1]
+        name_rows = [line.split('|')[1].strip() for line in lines]
+        name_rows = [name.split('/')[1] for name in name_rows]
+        # remove duplicates and keep the order
+        dmethod_name_list = list(dict.fromkeys(name_rows))
+    with open(f'output/summary_{metric}.txt', 'r') as f:
+        # retrieve dataset names
+        lines = f.readlines()
+        lines = lines[3:-1]
+        name_rows = [line.split('|')[1].strip() for line in lines]
+        name_rows = [name.split('/')[0] for name in name_rows]
+        # remove duplicates and keep the order
+        dataset_name_list = list(dict.fromkeys(name_rows))
+    print(f'methods: {method_name_list}, downstream methods: {dmethod_name_list}, datasets: {dataset_name_list}')
+    with open(f'output/summary_{metric}.txt', 'r') as f:
+        lines = f.readlines()[3:-1]
+        # remove \n at the end of each line
+        lines = [line[:-1] for line in lines]
+        # delete space in each line
+        lines = [line.replace(' ', '') for line in lines]
+        # remove '|' at the beginin and end of each line
+        lines = [line[1:-1] for line in lines]
+        # split each line by '|'
+        lines = [line.split('|') for line in lines]
+        # convert each element to float, 'x' should be converted to 0
+        table = [[0 if x == 'x' else float(x) for x in line[1:]] for line in lines]
+        # dataset_names = [line[0] for line in lines]
+        num_datasets = len(np.unique([line[0].split('/')[0] for line in lines]))
+        dataset_names = [line[0].split('/')[0] for line in lines][:num_datasets]
+        method_names = np.unique([line[0].split('/')[1] for line in lines])
+        print(dataset_names, method_names)
+        table = np.array(table)
+    table = table/100
+    num_dmethods = len(dmethod_name_list)
+    num_datasets = int(table.shape[0]/num_dmethods)
 
-os.makedirs('output/figs', exist_ok=True)
+    print(dataset_names)
+    for i, dataset in enumerate(dataset_names):
+        # gen indices of downstream methods for the current dataset
+        indices = [i+num_datasets*j for j in range(num_dmethods)]
+        t = table[indices]
+        # print(t.shape)
 
-metric = 'nmi'
+        # box plot
+        plt.style.use('classic')
+        plt.rcParams['pdf.fonttype'] = 42
+        plt.figure(figsize=(4.3, 4))
+        # plt.boxplot(avg_score_on_datasets.T, patch_artist=True, showfliers=False)
+        # use bold line for all the components
+        plt.boxplot(t.T,
+            patch_artist=True,
+            showfliers=False,
+            boxprops=dict(linewidth=1.5),
+            whiskerprops=dict(linewidth=1.5),
+            capprops=dict(linewidth=1.5),
+            medianprops=dict(linewidth=1.5))
+        # print(dmethod_name_list)
+        dmethod_name_list = [convert_name(name) for name in dmethod_name_list]
+        plt.xticks(range(1, num_dmethods+1), dmethod_name_list, fontsize=12)
+        plt.title(f'{dataset}', fontsize=16)
+        # plt.grid(axis='y', lw=2, linestyle='--', color='gray')
+        # plt.ylim(0.35, 0.85)
+        plt.ylabel('NMI', fontsize=16)
+        plt.xlabel('Downstream Methods', fontsize=16)
+        plt.tight_layout()
+        plt.savefig(f'output/figs/resilience_{dataset}_{metric}.pdf', bbox_inches='tight')
+        plt.savefig(f'output/figs/resilience_{dataset}_{metric}.png', bbox_inches='tight')
+        plt.close()
 
-with open(f'output/summary_{metric}.txt', 'r') as f:
-    lines = f.readlines()[3:-1]
-    # remove \n at the end of each line
-    lines = [line[:-1] for line in lines]
-    # delete space in each line
-    lines = [line.replace(' ', '') for line in lines]
-    # remove '|' at the beginin and end of each line
-    lines = [line[1:-1] for line in lines]
-    # split each line by '|'
-    lines = [line.split('|') for line in lines]
-    # convert each element to float, 'x' should be converted to 0
-    table = [[0 if x == 'x' else float(x) for x in line[1:]] for line in lines]
-    # dataset_names = [line[0] for line in lines]
-    num_datasets = len(np.unique([line[0].split('/')[0] for line in lines]))
-    dataset_names = [line[0].split('/')[0] for line in lines][:num_datasets]
-    method_names = np.unique([line[0].split('/')[1] for line in lines])
-    print(dataset_names, method_names)
-    table = np.array(table)
+    avg_score_on_datasets = []
+    for i in range(num_dmethods):
+        method_idx = [num_datasets*i+j for j in range(num_datasets)]
+        method_rows = table[method_idx]
+        # print(method_idx)
+        avg_score_on_datasets.append(np.mean(method_rows, axis=0))
+    avg_score_on_datasets = np.array(avg_score_on_datasets)
+    print(avg_score_on_datasets.shape)
 
-num_datasets = int(table.shape[0]/num_dmethod)
-
-avg_score_on_datasets = []
-for i in range(num_dmethod):
-    method_idx = [num_datasets*i+j for j in range(num_datasets)]
-    method_rows = table[method_idx]
-    # print(method_idx)
-    avg_score_on_datasets.append(np.mean(method_rows, axis=0))
-avg_score_on_datasets = np.array(avg_score_on_datasets)/100
-print(avg_score_on_datasets.shape)
-
-# box plot
-plt.style.use('classic')
-plt.rcParams['pdf.fonttype'] = 42
-plt.figure(figsize=(4.3, 4))
-# plt.boxplot(avg_score_on_datasets.T, patch_artist=True, showfliers=False)
-# use bold line for all the components
-plt.boxplot(avg_score_on_datasets.T,
-    patch_artist=True,
-    showfliers=False,
-    boxprops=dict(linewidth=1.5),
-    whiskerprops=dict(linewidth=1.5),
-    capprops=dict(linewidth=1.5),
-    medianprops=dict(linewidth=1.5))
-plt.xticks(range(1, num_dmethod+1), dmethod_names, fontsize=12)
-# plt.grid(axis='y', lw=2, linestyle='--', color='gray')
-plt.ylim(0.35, 0.85)
-plt.ylabel('NMI', fontsize=16)
-plt.xlabel('Downstream Methods', fontsize=16)
-plt.tight_layout()
-plt.savefig(f'output/figs/box_{metric}.pdf', bbox_inches='tight')
-
-# variance = np.var(avg_score_on_datasets, axis=1)
-# variance = [np.max(avg_score_on_datasets[i]) - np.min(avg_score_on_datasets[i]) for i in range(num_dmethod)]
-# variance = np.array(variance)
-# print(variance.shape)
-# plt.style.use('classic')
-# plt.figure(figsize=(5, 3))
-# plt.bar(dmethod_names, variance, color=['#c9393e', '#497fc0', '#29517c', '#9694e7'])
-# plt.savefig(f'output/figs/variance_{metric}.png', bbox_inches='tight')
+    # box plot
+    plt.style.use('classic')
+    plt.rcParams['pdf.fonttype'] = 42
+    plt.figure(figsize=(4.3, 4))
+    # plt.boxplot(avg_score_on_datasets.T, patch_artist=True, showfliers=False)
+    # use bold line for all the components
+    plt.boxplot(avg_score_on_datasets.T,
+        patch_artist=True,
+        showfliers=False,
+        boxprops=dict(linewidth=1.5),
+        whiskerprops=dict(linewidth=1.5),
+        capprops=dict(linewidth=1.5),
+        medianprops=dict(linewidth=1.5))
+    plt.xticks(range(1, num_dmethods+1), dmethod_name_list, fontsize=12)
+    # plt.grid(axis='y', lw=2, linestyle='--', color='gray')
+    # plt.ylim(0.35, 0.85)
+    plt.title(f'All Datasets', fontsize=16)
+    plt.ylabel('NMI', fontsize=16)
+    plt.xlabel('Downstream Methods', fontsize=16)
+    plt.tight_layout()
+    plt.savefig(f'output/figs/resilience_all_{metric}.pdf', bbox_inches='tight')
+    plt.savefig(f'output/figs/resilience_all_{metric}.png', bbox_inches='tight')
