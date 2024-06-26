5f118987eb0036085a2ec9fb0550a4f26bd4cfda
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-06-26 17:55:18 +0800

    plot new figure

--- tasks/draw_figures.sh
+++ tasks/draw_figures.sh
@@ -3,4 +3,6 @@ python experiments/summary.py
 python paper_figures/plot_cd.py
 python paper_figures/plot_overall_performance.py
 python paper_figures/plot_individual_performance.py
+python paper_figures/plot_method_box.py
+python paper_figures/plot_box.py
 python experiments/visualization.py
