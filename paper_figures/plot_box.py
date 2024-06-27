a4070db45d9bd8d68e5192e0469575bad6b43aee
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-06-27 17:08:48 +0800

    fix bug

--- paper_figures/plot_box.py
+++ paper_figures/plot_box.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-clf_name_list = ['ISSD', 'PCA', 'UMAP', 'ECP', 'ECS', 'LDA', 'SFM']
+# clf_name_list = ['ISSD', 'PCA', 'UMAP', 'ECP', 'ECS', 'LDA', 'SFM']
 dmethod_names = ['Time2State', 'TICC', 'E2USD', 'AutoPlait']
 dmethod_names_lower = ['time2state', 'ticc', 'e2usd', 'autoplait']
 # dataset_names = ['MoCap', 'ActRecTut', 'PAMAP2', 'USC-HAD', 'Synthetic']
@@ -13,6 +13,28 @@ num_dmethod = len(dmethod_names)
 os.makedirs('output/figs', exist_ok=True)
 
 for metric in ['ari', 'purity', 'nmi']:
+    with open(f'output/summary_{metric}.txt', 'r') as f:
+        # retrieve method names
+        lines = f.readlines()
+        method_name_list = lines[1].split('|')[2:-1]
+        method_name_list = [name.strip().upper() for name in method_name_list]
+    with open(f'output/summary_{metric}.txt', 'r') as f:
+        # retrieve downstream method names
+        lines = f.readlines()
+        lines = lines[3:-1]
+        name_rows = [line.split('|')[1].strip() for line in lines]
+        name_rows = [name.split('/')[1] for name in name_rows]
+        # remove duplicates and keep the order
+        dmethod_name_list = list(dict.fromkeys(name_rows))
+    with open(f'output/summary_{metric}.txt', 'r') as f:
+        # retrieve dataset names
+        lines = f.readlines()
+        lines = lines[3:-1]
+        name_rows = [line.split('|')[1].strip() for line in lines]
+        name_rows = [name.split('/')[0] for name in name_rows]
+        # remove duplicates and keep the order
+        dataset_name_list = list(dict.fromkeys(name_rows))
+    print(f'methods: {method_name_list}, downstream methods: {dmethod_name_list}, datasets: {dataset_name_list}')
     with open(f'output/summary_{metric}.txt', 'r') as f:
         lines = f.readlines()[3:-1]
         # remove \n at the end of each line
@@ -32,8 +54,8 @@ for metric in ['ari', 'purity', 'nmi']:
         print(dataset_names, method_names)
         table = np.array(table)
     table = table/100
-    print(table.shape)
-    num_datasets = int(table.shape[0]/num_dmethod)
+    num_dmethods = len(dmethod_name_list)
+    num_datasets = int(table.shape[0]/num_dmethods)
 
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(4, 3.2))
@@ -60,7 +82,7 @@ for metric in ['ari', 'purity', 'nmi']:
     # ax.set_yticks(np.arange(0, 1, 0.2))
     ax.set_ylim(0, 1.05)
     # ax.set_xticks(range(len(clf_name_list)), clf_name_list, rotation=45)
-    ax.set_xticklabels(clf_name_list, rotation=45)
+    ax.set_xticklabels(method_name_list, rotation=45)
     ax.set_ylabel(f'{metric.upper()}', fontsize=15)
     ax.yaxis.labelpad = -1
 
