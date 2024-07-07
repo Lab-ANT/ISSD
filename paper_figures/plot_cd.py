282a4df38eff036c6c5006190c443b5821624524
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-07-07 12:08:34 +0800

    use nemenyi two tailed

--- paper_figures/plot_cd.py
+++ paper_figures/plot_cd.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import os
+import scikit_posthocs as sp
 
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
@@ -292,10 +293,9 @@ def draw_cd_diagram(df_perf=None, alpha=0.05, title=None, labels=False):
     Draws the critical difference diagram given the list of pairwise classifiers that are
     significant or not
     """
-    p_values, average_ranks, _ = wilcoxon_holm(df_perf=df_perf, alpha=alpha)
-
-    print('p_values', average_ranks, p_values)
-
+    # p_values, average_ranks, _ = wilcoxon_holm(df_perf=df_perf, alpha=alpha)
+    p_values, average_ranks, _ = nemenyi_two_tailed(df_perf=df_perf, alpha=alpha)
+    # print('p_values', average_ranks, p_values)
     for p in p_values:
         print(p)
 
@@ -370,6 +370,80 @@ def wilcoxon_holm(alpha=0.05, df_perf=None):
         else:
             # stop
             break
+    # compute the average ranks to be returned (useful for drawing the cd diagram)
+    # sort the dataframe of performances
+    sorted_df_perf = df_perf.loc[df_perf['classifier_name'].isin(classifiers)]. \
+        sort_values(['classifier_name', 'dataset_name'])
+    # get the rank data
+    rank_data = np.array(sorted_df_perf['accuracy']).reshape(m, max_nb_datasets)
+
+    # create the data frame containg the accuracies
+    df_ranks = pd.DataFrame(data=rank_data, index=np.sort(classifiers), columns=
+    np.unique(sorted_df_perf['dataset_name']))
+
+    # number of wins
+    dfff = df_ranks.rank(ascending=False)
+    print(dfff[dfff == 1.0].sum(axis=1))
+
+    # average the ranks
+    average_ranks = df_ranks.rank(ascending=False).mean(axis=1).sort_values(ascending=False)
+    # return the p-values and the average ranks
+    return p_values, average_ranks, max_nb_datasets
+
+def nemenyi_two_tailed(alpha=0.05, df_perf=None):
+    """
+    Nemenyi two tailed rank test.
+    """
+    print(pd.unique(df_perf['classifier_name']))
+    # count the number of tested datasets per classifier
+    df_counts = pd.DataFrame({'count': df_perf.groupby(
+        ['classifier_name']).size()}).reset_index()
+    # get the maximum number of tested datasets
+    max_nb_datasets = df_counts['count'].max()
+    # get the list of classifiers who have been tested on nb_max_datasets
+    classifiers = list(df_counts.loc[df_counts['count'] == max_nb_datasets]
+                       ['classifier_name'])
+    # test the null hypothesis using friedman before doing a post-hoc analysis
+    friedman_p_value = friedmanchisquare(*(
+        np.array(df_perf.loc[df_perf['classifier_name'] == c]['accuracy'])
+        for c in classifiers))[1]
+    if friedman_p_value >= alpha:
+        # then the null hypothesis over the entire classifiers cannot be rejected
+        print('the null hypothesis over the entire classifiers cannot be rejected')
+        exit()
+    # get the number of classifiers
+    m = len(classifiers)
+    # init array that contains the p-values calculated by the Wilcoxon signed rank test
+    p_values = []
+    # loop through the algorithms to compare pairwise
+    for i in range(m - 1):
+        # get the name of classifier one
+        classifier_1 = classifiers[i]
+        # get the performance of classifier one
+        perf_1 = np.array(df_perf.loc[df_perf['classifier_name'] == classifier_1]['accuracy']
+                          , dtype=np.float64)
+        for j in range(i + 1, m):
+            # get the name of the second classifier
+            classifier_2 = classifiers[j]
+            # get the performance of classifier one
+            perf_2 = np.array(df_perf.loc[df_perf['classifier_name'] == classifier_2]
+                              ['accuracy'], dtype=np.float64)
+            # print(perf_1, perf_2)
+            test_data = np.array([perf_1, perf_2])
+            # print(test_data.shape)
+            result = sp.posthoc_nemenyi_friedman(test_data.T)
+            # result = sp.posthoc_nemenyi(test_data.T)
+            # print(result)
+            p_value = result.iloc[0, 1]
+            p_values.append((classifier_1, classifier_2, p_value, False))
+    # get the number of hypothesis
+    k = len(p_values)
+    # sort the list in acsending manner of p-value
+    p_values.sort(key=operator.itemgetter(2))
+
+    for i in range(k):
+        if p_values[i][2] <= alpha:
+            p_values[i] = (p_values[i][0], p_values[i][1], p_values[i][2], True)
     # compute the average ranks to be returned (useful for drawing the cd diagram)
     # sort the dataframe of performances
     sorted_df_perf = df_perf.loc[df_perf['classifier_name'].isin(classifiers)]. \
