f60da27aad6829b85d693756a370d9f53e7c6b5c
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-07-09 06:57:18 +0800

    add example for class version

--- usage_example.py
+++ usage_example.py
@@ -26,6 +26,7 @@ for fname in fname_list:
     data, state_seq = load_data(f'data/MoCap/raw/{fname}')
     datalist.append(data)
     state_seq_list.append(state_seq)
+selector = ISSD()
 selector.compute_matrices(datalist, state_seq_list)
 selected_channels_qf = selector.get_qf_solution(4)
 selected_channels_cf = selector.get_cf_solution(4)
