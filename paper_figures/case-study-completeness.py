b1098768f566003eb68709f617bd170ac1e19100
Chengyu Wang <chengyu@nudt.edu.cn/simple>
2024-10-17 17:04:38 +0800

    update figures

--- paper_figures/case-study-completeness.py
+++ paper_figures/case-study-completeness.py
@@ -2,31 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """
-Plot all
-"""
-# datasets = ['MoCap', 'ActRecTut', 'PAMAP2', 'USC-HAD', 'SynSeg']
-
-# for d in datasets:
-#     completeness = np.load(f'completeness_analysis/{d}_completeness.npy')
-#     quality = np.load(f'completeness_analysis/{d}_quality.npy')
-
-#     plt.style.use('classic')
-#     plt.figure(figsize=(10, 2))
-#     plt.imshow(completeness, cmap='viridis', interpolation='nearest')
-#     plt.colorbar()
-#     plt.tight_layout()
-#     plt.savefig(f'completeness_analysis/fig_{d}_completeness.png')
-#     plt.close()
-
-#     plt.style.use('classic')
-#     plt.figure(figsize=(10, 2))
-#     plt.imshow(quality, cmap='viridis', interpolation='nearest')
-#     plt.colorbar()
-#     plt.tight_layout()
-#     plt.savefig(f'completeness_analysis/fig_{d}_quality.png')
-#     plt.close()
-
-"""
 Plot MoCap
 """
 completeness = np.load('completeness_analysis/MoCap_completeness.npy')
@@ -40,7 +15,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # # Creating the plot with imshow
-fig, ax = plt.subplots(figsize=(3.8, 1))
+fig, ax = plt.subplots(figsize=(3.8, 0.9))
 
 # Plotting the imshow with rectangular data
 # cax = ax.imshow(completeness, aspect='auto', cmap='viridis')
@@ -49,8 +24,9 @@ cax = ax.pcolor(completeness, cmap='viridis', edgecolors='k', linewidths=0.5)
 for i in selected_channels:
     ax.add_patch(plt.Rectangle((i, 0), 1, completeness.shape[0], fill=False, edgecolor='red', lw=1))
 # ax.set_xlabel('Channel Index', fontsize=12)
-ax.set_ylabel('Index', fontsize=8)
+ax.set_ylabel('Time Series', fontsize=6)
 ax.set_yticks([])
+ax.set_xticks([])
 # ax.set_yticks(np.arange(0, completeness.shape[0], 2),[0,2,4,6,8])
 # set x tick size
 ax.tick_params(axis='x', labelsize=8)
@@ -80,10 +56,15 @@ fig, ax = plt.subplots(figsize=(3.8, 1))
 # Plotting the imshow with rectangular data
 # cax = ax.imshow(quality, aspect='auto', cmap='viridis')
 cax = ax.pcolor(quality, cmap='viridis', edgecolors='k', linewidths=0.5)
+# box the corresponding channels
+for i in selected_channels:
+    ax.add_patch(plt.Rectangle((i, 0), 1, completeness.shape[0], fill=False, edgecolor='red', lw=1))
 
 # ax.set_xlabel('Channel Index', fontsize=12)
-ax.set_ylabel('Index', fontsize=8)
+ax.set_ylabel('Time Series', fontsize=6)
+ax.set_xlabel('Channels', fontsize=6)
 ax.set_yticks([])
+ax.set_xticks([])
 # ax.set_yticks(np.arange(0, completeness.shape[0], 2),[0,2,4,6,8])
 # set x tick size
 ax.tick_params(axis='x', labelsize=8)
@@ -102,28 +83,3 @@ plt.tight_layout()
 plt.savefig('completeness_analysis/imshow_quality.png')
 plt.savefig('completeness_analysis/imshow_quality.pdf')
 plt.close()
-
-# # # Creating the plot with imshow
-# fig, ax = plt.subplots(figsize=(5, 1.5))
-
-# # Plotting the imshow with rectangular data
-# cax = ax.imshow(quality, aspect='auto', cmap='viridis')
-# # ax.set_xlabel('Channel Index', fontsize=12)
-# ax.set_ylabel('Instance Index', fontsize=10)
-# # ax.set_yticks(np.arange(0, quality.shape[0], 2),[0,2,4,6,8])
-
-# # Adding a colorbar and manually adjusting to match the height exactly
-# from mpl_toolkits.axes_grid1 import make_axes_locatable
-# divider = make_axes_locatable(ax)
-# cax_divider = divider.append_axes("right", size="2%", pad=0.1)
-
-# # Adding the colorbar to the side plot and setting the label
-# cbar = fig.colorbar(cax, cax=cax_divider)
-# # set the ticks of the colorbar, just use 'high' and 'low' to keep it simple
-# cbar.set_ticks([0, 1])
-# cbar.set_label('Quality')
-
-# plt.tight_layout()
-# plt.savefig('completeness_analysis/imshow_quality.png')
-# plt.savefig('completeness_analysis/imshow_quality.pdf')
-# plt.close()
--- paper_figures/dataset-completeness-quality.py
+++ paper_figures/dataset-completeness-quality.py
@@ -7,12 +7,11 @@ datasets = ['MoCap', 'ActRecTut', 'PAMAP2', 'USC-HAD', 'SynSeg']
 # quality = np.load('completeness_analysis/MoCap_quality.npy')
 
 plt.style.use('classic')
-fig, ax = plt.subplots(figsize=(4, 3.2))
+fig, ax = plt.subplots(figsize=(4, 3.3))
 table = []
 for d in datasets:
     completeness = np.load(f'completeness_analysis/{d}_completeness.npy').T
     # parts = ax.violinplot(completeness, showmeans=False, showmedians=True, showextrema=False)
-    # parts = 
     table.append(completeness.flatten())
 parts = ax.violinplot(table, showmeans=False, showmedians=True, showextrema=False)
 # violin setting
@@ -20,14 +19,17 @@ for pc in parts['bodies']:
     pc.set_facecolor('skyblue')
     pc.set_edgecolor('skyblue')
     pc.set_alpha(1)
-
-box = ax.boxplot(table, widths=0.25, patch_artist=True, whis=[0,100],)
-# plt.savefig('completeness_analysis/fig_completeness_box.png')
+box = ax.boxplot(table, widths=0.25, patch_artist=True, whis=[0,95])
+ax.set_xticks(np.arange(1, len(datasets)+1))
+ax.set_xticklabels(datasets, rotation=15, fontsize=10)
+ax.set_ylabel('Completeness')
+plt.subplots_adjust(left=0.15)
+plt.savefig('completeness_analysis/fig_completeness_box.png')
 plt.savefig('completeness_analysis/fig_completeness_box.pdf')
 plt.close()
 
 plt.style.use('classic')
-fig, ax = plt.subplots(figsize=(4, 3.2))
+fig, ax = plt.subplots(figsize=(4, 3.3))
 table = []
 for d in datasets:
     quality = np.load(f'completeness_analysis/{d}_quality.npy')
@@ -39,24 +41,10 @@ for pc in parts['bodies']:
     pc.set_edgecolor('skyblue')
     pc.set_alpha(1)
 box = ax.boxplot(table, widths=0.25, patch_artist=True)
-plt.tight_layout()
+ax.set_xticks(np.arange(1, len(datasets)+1))
+ax.set_xticklabels(datasets, rotation=15, fontsize=10)
+ax.set_ylabel('Quality')
+plt.subplots_adjust(left=0.15)
 plt.savefig('completeness_analysis/fig_quality_box.png')
 plt.savefig('completeness_analysis/fig_quality_box.pdf')
 plt.close()
-
-
-# plt.style.use('classic')
-# plt.figure(figsize=(10, 2))
-# plt.imshow(completeness, cmap='viridis', interpolation='nearest')
-# plt.colorbar()
-# plt.tight_layout()
-# plt.savefig('completeness_analysis/fig_completeness.png')
-# plt.close()
-
-# plt.style.use('classic')
-# plt.figure(figsize=(10, 2))
-# plt.imshow(quality, cmap='viridis', interpolation='nearest')
-# plt.colorbar()
-# plt.tight_layout()
-# plt.savefig('completeness_analysis/fig_quality.png')
-# plt.close()
